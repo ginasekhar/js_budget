c210d68d71f95c2d91c53a8da4b47971f3dd0595
Gina Sekhar <ginasekhar@yahoo.com>
2020-05-23 08:21:45 -0400

    set up data structures for income and expense

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 const budgetController = (function () {
-  
+  const Expense = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  }
+
+  const Income = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  }
+
+  let data = {
+    allItems : {
+      exp: [],
+      inc: []
+    },
+    totals: {
+      exp: 0,
+      inc: 0
+    }
+  }
 
 })();
 
